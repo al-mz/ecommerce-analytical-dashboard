4d4ed6af35c01582d89b35fe86128b7922d6ddef
ali-mhmzadeh <alimhmzadeh@gmail.com>
2022-08-07 00:37:52 +0000

    new dashboard added

--- boostrap.sh
+++ boostrap.sh
@@ -9,4 +9,4 @@ docker exec -ti -u root superset bash -c "sh ./docker-init.sh"
 # Open the browser Superset
 open http://localhost:8088
 
-echo "Success! Login to Superset using the credentials admin/admin"
+echo "Success! Login to http://localhost:8088 using the defaul admin/admin credentials"
