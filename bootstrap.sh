3b6ab002d94e1c41339dc8ef14d6e3611e537fb7
ali-mhmzadeh <alimhmzadeh@gmail.com>
2022-08-11 09:56:47 -0400

    updated

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Creates Olist PostgreSQL database
+docker exec -it -u root ecommerce_analytics sh -c "conda run --no-capture-output -n marketing_analysis python ./examples/olist.py"
+
+# Initializes the Superset application
+docker exec -ti -u root superset bash -c "sh ./docker-init.sh"
+
+# Open the browser Superset
+open http://localhost:8088
+
+echo "Success! Login to http://localhost:8088 using the defaul admin/admin credentials"
